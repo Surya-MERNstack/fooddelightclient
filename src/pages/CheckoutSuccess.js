c5a2c5ebbb60ce898e82e2671a95cebe363e839f
Surya-MERNstack <charusurya17@gmail.com>
2023-09-14 12:12:01 +0530

    first commit

---
+++ src/pages/CheckoutSuccess.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckoutSuccess = () => {
+  return (
+    <div>
+      <h3>Success</h3>
+    </div>
+  )
+}
+
+export default CheckoutSuccess
---
+++ src/utilits/ImageBase.js
@@ -0,0 +1,14 @@
+
+const Image = (file) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file)
+
+  const Data = new Promise((res,rej) => {
+    reader.onload = () => res(reader.result)
+    reader.onerror = e => rej.e
+  })
+  return Data
+}
+
+
+export {Image}
